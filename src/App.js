c3c2dae54bd1175f85f65ef650d28eb42c4f4d30
Anwar Louis <smmaniac@hotmail.co.uk>
2024-05-23 22:43:20 +0100

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>incscape reloaded! Coming soon!</p>
+        <div class="navbar">
+          <a href="/">Home</a>
+          <div class="dropdown">
+            <a href="/">Music</a>
+            <div class="dropdown-content">
+              <a href="https://anwarlouis.bandcamp.com/">Bandcamp</a>
+              <a href="https://www.last.fm/user/championanwar">Last FM</a>
+            </div>
+          </div>
+          <div class="dropdown">
+            <a href="/">Code</a>
+            <div class="dropdown-content">
+              <a href="https://github.com/inc-cat">GitHub</a>
+            </div>
+          </div>
+          <a href="/">Art</a>
+        </div>
       </header>
+      incspace reloaded, coming soon!
     </div>
   );
 }
