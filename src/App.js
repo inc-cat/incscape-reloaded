97e497854784d921b320eb06ae82af19a9f70d7f
Anwar Louis <smmaniac@hotmail.co.uk>
2024-05-27 23:44:48 +0100

    Refactor routing links

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 import './App.css';
-import SlideShow from './components/slideshow';
+import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
+import Blog from './components/blog';
+import Home from './components/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="navbar">
-          <a href="/">Home</a>
-          <div className="dropdown">
-            <a href="/">Music</a>
-            <div className="dropdown-content">
-              <a href="https://anwarlouis.bandcamp.com/">Bandcamp</a>
-              <a href="https://www.last.fm/user/championanwar">Last FM</a>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <div className="navbar">
+            <a href="/">Home</a>
+            <div className="dropdown">
+              <a href="/">Music</a>
+              <div className="dropdown-content">
+                <a href="https://anwarlouis.bandcamp.com/">Bandcamp</a>
+                <a href="https://www.last.fm/user/championanwar">Last FM</a>
+              </div>
             </div>
-          </div>
-          <div className="dropdown">
-            <a href="/">Code</a>
-            <div className="dropdown-content">
-              <a href="https://github.com/inc-cat">GitHub</a>
+            <div className="dropdown">
+              <a href="/">Code</a>
+              <div className="dropdown-content">
+                <a href="https://github.com/inc-cat">GitHub</a>
+              </div>
             </div>
+            <a href="/">Art</a>
+            <Link to="/blog">Blog</Link>
           </div>
-          <a href="/">Art</a>
+        </header>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<Blog />} />
+          </Routes>
         </div>
-      </header>
-      <SlideShow />
-      incspace reloaded, coming soon!
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
