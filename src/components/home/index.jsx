31a9388937396d984039a911ddf685cc3c1b4c27
Anwar Louis <smmaniac@hotmail.co.uk>
2024-06-13 21:04:17 +0100

    Add icons and knowledge panel to home page

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -8,12 +8,15 @@ function Home() {
   };
 
   const Knowledge = function (props) {
-    let { logo, label } = props;
+    let { logo, label, url } = props;
 
     return (
       <div class="grid-item flex-thing">
         <div className="logo">
-          <img src={logo} />
+          <a href={url}>
+            {' '}
+            <img src={logo} />{' '}
+          </a>
         </div>
         <div className="label">{label}</div>
       </div>
@@ -24,10 +27,42 @@ function Home() {
     <>
       <SlideShow />
       <ManageBio />
-      incspace reloaded, coming soon!
+
+      <h1 className="headings">Music</h1>
       <KnowledgeGrid>
-        <Knowledge logo="icons/fl.png" label="FL Studio" />
-        <Knowledge logo="icons/protools.png" label="Pro Tools" />
+        <Knowledge
+          logo="icons/fl.png"
+          label="FL Studio"
+          url="https://www.image-line.com/"
+        />
+        <Knowledge
+          logo="icons/protools.png"
+          label="Pro Tools"
+          url="https://www.avid.com/pro-tools"
+        />
+      </KnowledgeGrid>
+      <h1 className="headings">Programming</h1>
+      <KnowledgeGrid>
+        <Knowledge
+          logo="icons/js.png"
+          label="JavaScript"
+          url="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        />
+        <Knowledge
+          logo="icons/python.png"
+          label="Python"
+          url="https://www.python.org/"
+        />
+        <Knowledge
+          logo="icons/html5.png"
+          label="HTML5"
+          url="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+        />
+        <Knowledge
+          logo="icons/react.png"
+          label="React"
+          url="https://react.dev/"
+        />
       </KnowledgeGrid>
     </>
   );
